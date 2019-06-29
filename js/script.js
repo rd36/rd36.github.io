9938befca83c1560e8902b55b2d732448ae988f9
Кирилов Андрей <akirilov@cinimex.ru>
2019-06-29 15:52:41 +0300

    epic fix

--- js/script.js
+++ js/script.js
@@ -1,55 +1,37 @@
-if (device.mobile()) {
-    document.getElementById("css").href = "css/mobile.css";
-}
-var setVisibility = function (elementName, isVisible) {
-    var currentElement = document.querySelector("." + elementName + "-container");
-    if (isVisible) {
-        currentElement.style.display = "inline";
-        setTimeout(function () {
-            currentElement.style.opacity = 1;
-            currentElement.style.visibility = "visible";
-        }, 20);
-    } else {
-        currentElement.style.display = "none";
-        setTimeout(function () {
-            currentElement.style.opacity = 0;
-            currentElement.style.visibility = "hidden";
-        }, 20);
+if (!device.mobile()) {
+    document.getElementsByTagName("html")[0].style.margin = "0px 43px 63px 17px";
+    document.getElementsByTagName("body")[0].style.textAlign = "justify";
+
+    var table = document.getElementsByTagName("table");
+    for (var i = 0; i < table.length; i++) {
+        table[i].style.fontSize = "1em";
+        table[i].style.marginBottom = "8px";
     }
-};
-var articlesName = "articles";
-var articlesArray = ["reestr", "meeting", "tszh", "uk", "services"];
-var redirectPage = function (pageId) {
-    var aCurrentLast = document.querySelector(".a-current");
-    aCurrentLast.classList.remove("a-current");
-    setVisibility(aCurrentLast.id, false);
-    if (aCurrentLast.id === articlesName) {
-        for (i = 0; i < articlesArray.length; i++) {
-            setVisibility(articlesArray[i], false);
-        }
+
+    var td = document.getElementsByTagName("td");
+    for (var i = 0; i < td.length; i++) {
+        td[i].style.verticalAlign = "middle";
     }
-    document.getElementById((articlesArray.indexOf(pageId) > -1) ? articlesName : pageId).classList.add("a-current");
-    setVisibility(pageId, true);
-};
-var inputs = document.getElementsByTagName("a");
-for (i = 0; i < inputs.length; i++) {
-    if (inputs[i].hasAttribute("id")) {
-        inputs[i].addEventListener("click", function (evt) {
-            evt.preventDefault();
-            history.pushState(null, "", evt.target.href);
-            redirectPage(this.id);
-            document.body.scrollTop = document.documentElement.scrollTop = 0;
-        }, false);
+
+    var headLeft = document.getElementsByClassName("head-left")[0];
+    if (headLeft) {
+        headLeft.style.float = "left";
+        headLeft.style.width = "28%";
+    }
+
+    var headRight = document.getElementsByClassName("head-right")[0];
+    if (headRight) {
+        headRight.style.float = "left";
+        headRight.style.width = "72%";
+    }
+
+    var articleContainer = document.getElementsByClassName("article-container")[0];
+    if (articleContainer) {
+        articleContainer.style.padding = "0px 150px";
+    }
+
+    var footer = document.getElementsByClassName("footer")[0];
+    if (footer) {
+        footer.style.fontSize = "1.2em";
     }
 }
-var setCurrentPage = function () {
-    var currentHrefId = window.location.href.split("/")[window.location.href.split("/").length - 1];
-    if (currentHrefId === "") {
-        currentHrefId = "main";
-    }
-    redirectPage(currentHrefId);
-};
-window.onpopstate = function () {
-    setCurrentPage();
-};
-setCurrentPage();
